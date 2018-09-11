d996ae0a7229a960a23cec4ddd8b8a7e1017da70
Jess <jessica.gilbert@softwire.com>
2018-09-11 15:40:22 +0100

    No bug/ No answer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,33 +1,24 @@
 package com.company;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("Hello World!");
-        Scanner input = new Scanner(new File("test123.txt"));
-        String answer = input.nextLine();
-        System.out.println(answer);
+    private static CharSequence inputText;
+
+    public static void main(String[] args) {
 	// write your code here
         Path path = Paths.get("C:\\Users\\JJG\\Work\\Training\\EmailExtraction\\sample.txt");
         fileOpen(path);
         {
 
             Pattern pattern = Pattern.compile("\\w*@softwire.com\\s");
-            CharSequence inputText;
-            Matcher matcher = pattern.matcher(inputText);
-
-            matcher.find();
-            matcher.group();
-            matcher.group(int start);
+            Matcher matcher = pattern.matcher("\\w*@softwire.com\\s");
 
 
         }
